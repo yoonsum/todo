837f5d02ee5025eacf98e46dae1a190b34799ae0
yoonsum <77226422+yoonsum@users.noreply.github.com>
2024-01-18 00:58:52 +0900

    테스트 수정 및 회원정보수정 테스트 추가 - Member domain -> MemberDTO 사용 - 회원 정보 수정 테스트

--- src/test/java/com/example/todoapp/service/MemberServiceTest.java
+++ src/test/java/com/example/todoapp/service/MemberServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.todoapp.service;
 
 import com.example.todoapp.domain.Member;
+import com.example.todoapp.dto.MemberDTO;
+import com.example.todoapp.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,27 +18,38 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Transactional
 class MemberServiceTest {
     @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void 회원가입() throws Exception{
         //given
-        Member member = new Member("user1", "user1", "user1pw");
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setMemberId("user1");
+        memberDTO.setMemberName("user1");
+        memberDTO.setPassword("user1pw");
 
         //when
-        String id = memberService.join(member);
+        String id = memberService.join(memberDTO);
 
         //then
-        assertThat(id).isEqualTo(member.getId());
+        assertThat(id).isEqualTo(memberDTO.getMemberId());
     }
 
     @Test
     public void 중복_회원_예외() throws Exception{
         //given
-        Member member1 = new Member("user1", "user1", "user1pw");
-        Member member2 = new Member("user1", "user1", "user1pw");
+        MemberDTO member1 = new MemberDTO();
+        member1.setMemberId("user1");
+        member1.setMemberName("user1");
+        member1.setPassword("user1pw");
+        String id1 = memberService.join(member1);
+
+        MemberDTO member2 = new MemberDTO();
+        member2.setMemberId("user1");
+        member2.setMemberName("user1");
+        member2.setPassword("user1pw");
 
         //when
-        String id1 = memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
                 ()->memberService.join(member2));
 
@@ -44,24 +60,35 @@ class MemberServiceTest {
     @Test
     public void 로그인() throws Exception{
         //given
-        Member member1 = new Member("user1", "user1", "user1pw");
+        MemberDTO member1 = new MemberDTO();
+        member1.setMemberId("user1");
+        member1.setMemberName("user1");
+        member1.setPassword("user1pw");
+        String memberId = memberService.join(member1);
 
         //when
-        String login_id = memberService.login(member1.getId(), member1.getPassword());
+        String login_id = memberService.login(member1.getMemberId(), member1.getPassword());
 
-        assertThat(login_id).isEqualTo(member1.getId());
+        assertThat(login_id).isEqualTo(memberId);
     }
 
     @Test
     public void 존재하지_않는_회원() throws Exception{
         //given
-        Member member1 = new Member("user1", "user1", "user1pw");
+        MemberDTO member1 = new MemberDTO();
+        member1.setMemberId("user1");
+        member1.setMemberName("user1");
+        member1.setPassword("user1pw");
         memberService.join(member1);
 
-        Member login_member = new Member("user", "user1", "user1pw");
-
         //when
-        IllegalStateException e = assertThrows(IllegalStateException.class,
+        Member login_member = Member.builder()
+                .id("user")
+                .name("user1")
+                .password("user1pw")
+                .build();
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
                 ()->memberService.login(login_member.getId(), login_member.getPassword()));
 
         //then
@@ -71,16 +98,51 @@ class MemberServiceTest {
     @Test
     public void 로그인_실패() throws Exception{
         //given
-        Member member1 = new Member("user1", "user1", "user1pw");
+        MemberDTO member1 = new MemberDTO();
+        member1.setMemberId("user1");
+        member1.setMemberName("user1");
+        member1.setPassword("user1pw");
         memberService.join(member1);
 
-        Member login_member = new Member("user1", "user1", "pw");
-
         //when
-        IllegalStateException e = assertThrows(IllegalStateException.class,
+        Member login_member = Member.builder()
+                .id("user1")
+                .name("user1")
+                .password("pw")
+                .build();
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
                 ()->memberService.login(login_member.getId(), login_member.getPassword()));
 
         //then
         assertThat(e.getMessage()).isEqualTo("로그인 실패. 아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
+
+    @Test
+    @Rollback(value = false)
+    public void 회원_정보_수정() throws Exception{
+        //given
+        MemberDTO member1 = new MemberDTO();
+        member1.setMemberId("user1");
+        member1.setMemberName("user1");
+        member1.setPassword("user1pw");
+        memberService.join(member1);
+
+        //when
+        MemberDTO updateMember = new MemberDTO();
+        updateMember.setMemberId("user1");
+        updateMember.setMemberName("userUpdate");
+        updateMember.setPassword("userUpdatePW");
+
+        memberService.updateMember(updateMember);
+
+        //then
+        Optional<Member> optionalMember = memberRepository.findById(updateMember.getMemberId());
+        if(optionalMember.isPresent()){
+            Member member = optionalMember.get();
+            assertThat(member.getName()).isEqualTo(updateMember.getMemberName());
+            assertThat(member.getPassword()).isEqualTo(updateMember.getPassword());
+        }
+
     }
 }
