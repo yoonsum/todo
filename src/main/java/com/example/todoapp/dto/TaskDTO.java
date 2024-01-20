53c14166505a15e373d5adc2130144f3bc51aabe
yoonsum <77226422+yoonsum@users.noreply.github.com>
2024-01-20 16:02:58 +0900

    task DTO 생성

---
+++ src/main/java/com/example/todoapp/dto/TaskDTO.java
@@ -0,0 +1,23 @@
+package com.example.todoapp.dto;
+
+import com.example.todoapp.domain.Label;
+import com.example.todoapp.domain.TaskPriority;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class TaskDTO {
+    @NotEmpty(message = "title은 필수입니다.")
+    private String title;
+
+    private String content;
+
+    private LocalDate taskDate;
+
+    private TaskPriority taskPriority;
+
+    private Label label;
+}
